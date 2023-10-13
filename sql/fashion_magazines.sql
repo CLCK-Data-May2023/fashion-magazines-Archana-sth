eee3148ce7e63fa3732d75855f00aaacecfc6c27
Archana Khaiyarguli <archanasth765@gmail.com>
2023-10-12 21:07:33 -0400

    Fashion Magazine

--- sql/fashion_magazines.sql
+++ sql/fashion_magazines.sql
@@ -1,1 +1,16 @@
 -- Add your sql here
+--PRINTF() function to format the total_amount_due as currency, ensuring it displays two decimal places and $.
+select customers.customer_name as Customer, 
+ PRINTF("$%.2f",sum(subscriptions.price_per_month * subscriptions.subscription_length)) as "Amount Due"
+
+from orders
+inner join customers
+ on customers.customer_id = orders.customer_id
+
+inner join subscriptions
+ on subscriptions.subscription_id = orders.subscription_id
+
+where subscriptions.description = "Fashion Magazine"
+ and orders.order_status = "unpaid"
+
+group by Customer;
