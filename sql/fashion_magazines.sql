2c07769ec4a7cb453f5bc062376029b8bf4dc24f
Archana Khaiyarguli <archanasth765@gmail.com>
2023-10-12 22:06:53 -0400

    changed2

--- sql/fashion_magazines.sql
+++ sql/fashion_magazines.sql
@@ -1,8 +1,10 @@
 -- Add your sql here
 --PRINTF() function to format the total_amount_due as currency, ensuring it displays two decimal places and $.
 select customers.customer_name as Customer, 
- PRINTF("$%.2f",sum(subscriptions.price_per_month * subscriptions.subscription_length)) as "Amount Due"
-
+ PRINTF("$%.2f",sum(subscriptions.price_per_month * subscriptions.subscription_length)) 
+ as "Amount Due"
+ 
+/* inner join orders to customers */
 from orders
 inner join customers
  on customers.customer_id = orders.customer_id
